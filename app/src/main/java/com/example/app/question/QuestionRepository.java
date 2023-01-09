7c48d293296b19c202f1479ac3fbe1a3e04cdee2
JorimJoram <107216416+JorimJoram@users.noreply.github.com>
2023-01-09 21:35:24 +0900

    Delete app directory

--- app/src/main/java/com/example/app/question/QuestionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.app.question;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-}
