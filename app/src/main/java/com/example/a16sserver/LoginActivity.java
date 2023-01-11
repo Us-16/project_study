ad50f5749cf3995d052fe51150c2c103eedc03c5
skbna <bbbisskk2@naver.com>
2023-01-11 22:32:48 +0900

    로그인 창 0111 09:58

--- app/src/main/java/com/example/a16sserver/LoginActivity.java
+++ app/src/main/java/com/example/a16sserver/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.a16sserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageButton btn_kakaologin = (ImageButton) findViewById(R.id.btn_login);
+
+        btn_kakaologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), User_account.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/a16sserver/User_account.java
@@ -0,0 +1,14 @@
+package com.example.a16sserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_account);
+    }
+}
