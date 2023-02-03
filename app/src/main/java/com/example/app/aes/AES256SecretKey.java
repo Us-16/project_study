9e613b4200bb742894acadd8d3025647458cf39e
JorimJoram <107216416+JorimJoram@users.noreply.github.com>
2023-02-03 20:12:25 +0900

    Delete AES256SecretKey.java

--- app/src/main/java/com/example/app/aes/AES256SecretKey.java
+++
@@ -1,6 +0,0 @@
-package com.example.app.aes;
-
-public interface AES256SecretKey {
-    public final String key = "janghyorimjorimjoramBaseballSoli";
-    public final String iv = key.substring(4, 20);
-}
