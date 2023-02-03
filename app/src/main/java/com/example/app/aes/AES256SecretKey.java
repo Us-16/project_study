f44554f64f349b6bef269123cd7de24f15a1e234
janghyorim <wkdgyfla97@gmail.com>
2023-02-03 20:17:28 +0900

    !!!GIT Test8!!! - key.class ignore

--- app/src/main/java/com/example/app/aes/AES256SecretKey.java
+++ app/src/main/java/com/example/app/aes/AES256SecretKey.java
@@ -2,5 +2,5 @@ package com.example.app.aes;
 
 public interface AES256SecretKey {
     public final String key = "janghyorimjorimjoramBaseballSoli"; //32 letters
-    public final String iv = key.substring(4, 20); // 16
+    public final String iv = key.substring(4, 20); // 16bytes
 }
