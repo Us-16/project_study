1aaa294d0da949f0c3b599c8babbe3504b007605
janghyorim <wkdgyfla97@gmail.com>
2023-02-03 20:12:03 +0900

    !!!GIT Test6!!! - key.class ignore

---
+++ app/src/main/java/com/example/app/aes/AES256SecretKey.java
@@ -0,0 +1,6 @@
+package com.example.app.aes;
+
+public interface AES256SecretKey {
+    public final String key = "janghyorimjorimjoramBaseballSoli";
+    public final String iv = key.substring(4, 20);
+}
