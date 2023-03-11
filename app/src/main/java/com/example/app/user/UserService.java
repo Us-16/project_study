8b911a13217a862953cdbe52aa4b2c9318c1b599
JorimJoram <wkdgyfla97@gmail.com>
2023-03-11 14:12:13 +0900

    github are you alright?

--- app/src/main/java/com/example/app/user/UserService.java
+++ app/src/main/java/com/example/app/user/UserService.java
@@ -93,7 +93,7 @@ public class UserService {
         if(this.studentRepository.findByUsername(username).equals(Optional.empty())) {
             return "true";
         }
-        return "false";
+        return "false"; //check
     }
 
 }
