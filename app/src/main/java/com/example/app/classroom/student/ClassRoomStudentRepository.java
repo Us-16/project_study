7540cc45ab43e5cbd23cb12ab64523266e42cb6a
JorimJoram <wkdgyfla97@gmail.com>
2023-03-15 18:51:41 +0900

    add classroom for students

---
+++ app/src/main/java/com/example/app/classroom/student/ClassRoomStudentRepository.java
@@ -0,0 +1,6 @@
+package com.example.app.classroom.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassRoomStudentRepository extends JpaRepository<ClassRoomStudent, Long> {
+}
